22efd5689e192e9317fd5814a3126afb2da8c19c
Tran Quang Huy <tranquanghuy341@gmail.com>
2017-05-19 22:39:18 -0400

    Update them nguoi online

--- index.js
+++ index.js
@@ -8,21 +8,41 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 server.listen(3001);
 
-
-
-io.on('connection', function(socket) {
+var usersList = []
+var usersList1 = []
+io.on('connection', function (socket) {
     console.log('Co nguoi ket noi ' + socket.id);
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log(`${socket.id} vua ngat ket noi`)
     })
-    socket.on('client-send-data', function(data){
-        console.log(`Client says ${data}`)
-        socket.emit('server-send-data', data+'123')
+    socket.on('Client-send-reg', function (data) {
+        // console.log(`Client says ${data}`)
+        socket.userName = data
+
+        if (usersList.indexOf(socket.userName) >= 0) {
+
+            socket.emit('server-send-thatbai', data)
+        } else {
+            usersList.push(data)
+            socket.emit('server-send-thanhcong', data)
+            io.sockets.emit('server-send-thanhcong-moinguoi', usersList)
+            
+        }
+        socket.on('logout', function(){
+            // console.log(usersList.indexOf(socket.userName))
+            if (usersList.indexOf(socket.userName)>=0){
+            usersList.splice(usersList.indexOf(socket.userName), 1);
+            socket.broadcast.emit('server-send-thanhcong-moinguoi', usersList)
+                
+            }
+        })
+        
     })
-    
+
 
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index');
+    // res.send('abc');
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,32 @@
 var socket = io('http://localhost:3001')
+socket.on('server-send-thatbai', function(data){
+    alert(`${data} có người sử dụng rồi`)
+})
+socket.on('server-send-thanhcong', function(data){
+    $(".loginForm").hide('2000');
+    $(".chatForm").show('1000');
+    $("#currentUser").append(data)  
+    //  $('#listUsers').append(data)
+})
+socket.on('server-send-thanhcong-moinguoi', function(data){
+    // var $newdiv1 = $( "<p></p>" ),
 
+     $('#listUsers').html("")
+     
+     data.forEach(function(i){
+     $('#listUsers').append(`<div class="users">${i}</div>`)
+     })
+})
 $(document).ready(function(){
     $(".loginForm").show();
-    
+    // alert('avc')
+    $("#btnRegister").click(function(){
+        socket.emit('Client-send-reg', $("#txtUserName").val());
+    })
+    $("#btnLogout").click(function(){
+        socket.emit('logout');
+        $(".chatForm").hide('1000');
+    $(".loginForm").show('2000');
+              
+    })
 })
