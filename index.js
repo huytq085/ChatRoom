7d6c8d98aa2540780c262613c3608d81c67f4fa2
Tran Quang Huy <tranquanghuy341@gmail.com>
2017-05-21 00:34:35 -0400

    update encrypt, typing

--- index.js
+++ index.js
@@ -19,28 +19,44 @@ io.on('connection', function (socket) {
         // console.log(`Client says ${data}`)
         socket.userName = data
 
-        if (usersList.indexOf(socket.userName) >= 0) {
+        if (usersList.indexOf(data) >= 0) {
 
             socket.emit('server-send-thatbai', data)
         } else {
             usersList.push(data)
+            console.log(data)
             socket.emit('server-send-thanhcong', data)
             io.sockets.emit('server-send-thanhcong-moinguoi', usersList)
-            
+
         }
-        socket.on('logout', function(){
+        socket.on('logout', function () {
             // console.log(usersList.indexOf(socket.userName))
-            if (usersList.indexOf(socket.userName)>=0){
-            usersList.splice(usersList.indexOf(socket.userName), 1);
-            socket.broadcast.emit('server-send-thanhcong-moinguoi', usersList)
-                
+            if (usersList.indexOf(socket.userName) >= 0) {
+                usersList.splice(usersList.indexOf(socket.userName), 1);
+                socket.broadcast.emit('server-send-thanhcong-moinguoi', usersList)
+                socket.emit('server-logout')
             }
+            socket.emit('server-logout')
         })
-        socket.on('user-send-message', function(data){
-            io.sockets.emit('server-send-message', {"username": socket.userName, "message": data})
+        //Server nhan tin nhan duoc ma hoa
+        socket.on('user-send-message', function (data) {
+            console.log("user-send-message" + data)
+            io.sockets.emit('server-send-message', { "username": socket.userName, "message": data })
             console.log(data)
         })
+        socket.on('user-send-message-noencrypt', function (data) {
+            console.log("user-send-message-noencrypt" + data)
+            io.sockets.emit('server-send-message-noencrypt', { "username": socket.userName, "message": data })
+            console.log(data)
+        })
+        socket.on('dang-go-phim', function(){
+            socket.broadcast.emit('co-nguoi-go-phim', socket.userName)
+        })
+        socket.on('ngung-go-phim', function(){
+            socket.broadcast.emit('khong-con-go-phim')
+        })
         
+
     })
 
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
 var socket = io('http://localhost:3001')
+
 socket.on('server-send-thatbai', function (data) {
     alert(`${data} có người sử dụng rồi`)
 })
@@ -7,6 +8,11 @@ socket.on('server-send-thanhcong', function (data) {
     $(".chatForm").show('1000');
     $("#currentUser").append(`<span style="font-weight: 700;">${data}</span>`)
     //  $('#listUsers').append(data)
+})
+socket.on('server-logout', function () {
+    
+    $("#currentUser").html('')
+    // alert('a')
 })
 socket.on('server-send-thanhcong-moinguoi', function (data) {
     // var $newdiv1 = $( "<p></p>" ),
@@ -17,8 +23,22 @@ socket.on('server-send-thanhcong-moinguoi', function (data) {
         $('#listUsers').append(`<div class="users">${i}</div>`)
     })
 })
+//Nguoi dung nhan tin nhan da duoc giai ma
 socket.on('server-send-message', function (data) {
+    //Giai ma tin nhan truoc khi gui
+    var decrypted = CryptoJS.AES.decrypt(data.message, "Secret Passphrase");
+    $("#listMessages").append(`<div class="listMessages"><span style="font-weight:700">${data.username}</span>: ${decrypted.toString(CryptoJS.enc.Utf8)}</div>`)
+    // $("#listMessages").append(`<div class="listMessages"><span style="font-weight:700">${data.username}</span>: ${data.message}</div>`)
+})
+//Nguoi dung nhan tin nhan da duoc ma hoa nhung chua giai ma
+socket.on('server-send-message-noencrypt', function (data) {
     $("#listMessages").append(`<div class="listMessages"><span style="font-weight:700">${data.username}</span>: ${data.message}</div>`)
+})
+socket.on('co-nguoi-go-phim',function(data){
+$("#gophim").html(`<img src="/img/typing.gif" style="width: 30px"/><b>${data}</b> đang nhập văn bản`)
+})
+socket.on('khong-con-go-phim',function(data){
+$("#gophim").html('')
 })
 $(document).ready(function () {
     $(".loginForm").show();
@@ -33,13 +53,28 @@ $(document).ready(function () {
 
     })
     $("#btnSendMessage").click(function () {
-        socket.emit('user-send-message', $('#txtMessage').val());
+        var encrypted = CryptoJS.AES.encrypt($("#txtMessage").val(), "Secret Passphrase");
+        socket.emit('user-send-message', encrypted.toString());
+        $('#txtMessage').val('')
+
+        // socket.emit('user-send-message', $("#txtMessage").val());
+    })
+    $("#btnNoEncrypt").click(function () {
+        //Truoc khi gui di, ma hoa tin nhan
+        var encrypted = CryptoJS.AES.encrypt($("#txtMessage").val(), "Secret Passphrase");
+        socket.emit('user-send-message-noencrypt', encrypted.toString());
+        $('#txtMessage').val('')
+
+        // Chay binh thuong khong ma hoa du lieu
+        // socket.emit('user-send-message-noencrypt', $("#txtMessage").val());
     })
     $('#txtMessage').keydown(function (event) {
         var keypressed = event.keyCode || event.which;
         if (keypressed == 13) {
-            socket.emit('user-send-message', $('#txtMessage').val());
+            var encrypted = CryptoJS.AES.encrypt($("#txtMessage").val(), "Secret Passphrase");
+            socket.emit('user-send-message', encrypted.toString());
             $('#txtMessage').val('')
+            // socket.emit('user-send-message', $('#txtMessage').val());
         }
     });
     $('#txtUserName').keydown(function (event) {
@@ -48,4 +83,10 @@ $(document).ready(function () {
             socket.emit('Client-send-reg', $("#txtUserName").val());
         }
     });
+    $('#txtMessage').focusin(function(){
+        socket.emit('dang-go-phim')
+    })
+    $('#txtMessage').focusout(function(){
+        socket.emit('ngung-go-phim')
+    })
 })
